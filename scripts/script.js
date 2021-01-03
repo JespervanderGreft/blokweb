cef9729401bcc79f0f627c34d588c0bd6a77db3a
JespervanderGreft <45423847+JespervanderGreft@users.noreply.github.com>
2021-01-03 21:49:55 +0100

    responsive gemaakt en javascript begonnen
    Javascript begonnen maar ik liep vast

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,8 @@
 // JavaScript Document
+// ervoor zorgen dat de reacties die er al staan verdwijnen wanneer de details wordt opengeklapt
+var opmerkingenButton = document.querySelector("details");
+
+opmerkingenButton.addEventListener("toggle", function verwijderReacties() {
+        let BestaandeReacties = document.querySelector("body .homepage article>footer>article");
+        BestaandeReacties.classList.toggle("verwijder");
+   })
