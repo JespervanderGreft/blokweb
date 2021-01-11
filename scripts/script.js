e4e15973c43aacd760b773448a4ad37fdc87dcdd
JespervanderGreft <45423847+JespervanderGreft@users.noreply.github.com>
2021-01-12 00:30:35 +0100

    like animatie toegevoegd

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 // JavaScript Document
 // ervoor zorgen dat de reacties die er al staan verdwijnen wanneer de details wordt opengeklapt
-var opmerkingenButton = document.querySelector("details");
+var buttonLikeGroot = document.querySelector("button.like-button-groot");
+buttonLikeGroot.addEventListener("click", liken);
 
-opmerkingenButton.addEventListener("toggle", function verwijderReacties() {
-        let BestaandeReacties = document.querySelector("body .homepage article>footer>article");
-        BestaandeReacties.classList.toggle("verwijder");
-   })
+function liken() {
+        buttonLikeGroot.classList.toggle("geliked");
+}
