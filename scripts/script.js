3a1e90f7ace619e1f720f4184d4cd832afdcb2d4
JespervanderGreft <45423847+JespervanderGreft@users.noreply.github.com>
2021-01-12 14:18:53 +0100

    Like animatie
    Ik heb geprobeerd de like animatie naar elke button te laten luister met querySelectorAll maar helaas lukte dit niet en ik heb echt alles geprobeerd voor de rest maar ik snap niet waarom het niet werkt op verschillende manieren.

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,17 @@
 // JavaScript Document
 // ervoor zorgen dat de reacties die er al staan verdwijnen wanneer de details wordt opengeklapt
 var buttonLikeGroot = document.querySelector("button.like-button-groot");
-buttonLikeGroot.addEventListener("click", liken);
+buttonLikeGroot.addEventListener("click", grootliken);
 
-function liken() {
-        buttonLikeGroot.classList.toggle("geliked");
+function grootliken() {
+        buttonLikeGroot.classList.toggle("grootGeliked");
+        console.log("Hello world!");
+}
+
+var buttonLikeKlein = document.querySelector("button.like");
+buttonLikeKlein.addEventListener("click", kleinliken);
+
+function kleinliken() {
+        buttonLikeKlein.classList.toggle("kleinGeliked");
+        console.log("Hello world!");
 }
