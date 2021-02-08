5cecb76d5ad85618af75710584ef5d0e54ea2247
JespervanderGreft <45423847+JespervanderGreft@users.noreply.github.com>
2021-02-08 10:06:39 +0100

    Like buttons javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,21 @@
 // JavaScript Document
 // Zorgt ervoor dat de class veranderd wordt van de like button en die triggerd meteen ook een animatie
-var buttonLikeGroot = document.querySelector("button.like-button-groot");
+var buttonLikeGroot = document.querySelectorAll('.like-button-groot')
 
-// for (i = 0; i < buttonLikeGroot.length; i++) {
-//         buttonLikeGroot[i].addEventListener("click", grootliken);
-//       }
+for (i = 0; i < buttonLikeGroot.length; i++) {
+        buttonLikeGroot[i].addEventListener("click", grootLiken)
+      }
 
-buttonLikeGroot.addEventListener("click", grootliken);
-
-function grootliken() {
-        buttonLikeGroot.classList.toggle("grootGeliked");
+function grootLiken(e) {
+        e.target.classList.toggle('grootGeliked')
 }
 
-// var buttonLikeKlein = document.querySelector("button.like");
-// buttonLikeKlein.addEventListener("click", kleinliken);
+var buttonLikeKlein = document.querySelectorAll('.like-button-klein')
 
-// function kleinliken() {
-//         buttonLikeKlein.classList.toggle("kleinGeliked");
-//         console.log("Hello world!");
-// }
+for (i = 0; i < buttonLikeKlein.length; i++) {
+        buttonLikeKlein[i].addEventListener("click", kleinLiken)
+      }
+
+function kleinLiken(e) {
+        e.target.classList.toggle('kleinGeliked')
+}
